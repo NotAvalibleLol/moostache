5f1dc05951f8695bafa881910ded0e9d67ebcdfc
NotAvalibleLol <99042608+NotAvalibleLol@users.noreply.github.com>
2022-10-03 17:32:33 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded (){
+    console.log('PoseNet Is Intialized');
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
