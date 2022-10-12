9cb056ff9a6febe2637e30f5adf1e707848ef55c
NotAvalibleLol <99042608+NotAvalibleLol@users.noreply.github.com>
2022-10-12 17:26:36 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX = 0;
+noseY = -10;
 function preload() {
-
+    moustache = loadImage('https://i.postimg.cc/sDWvQ25F/m.png')
 }
 
 function setup() {
@@ -18,9 +20,23 @@ function modelLoaded (){
 }
 
 function draw(){
-
+    image(video, 0, 0, 300, 300);
+    image(moustache, noseX, noseY, 70, 50);
 }
 
 function take_snapshot(){
     save('myFilterImage.png');
+}
+
+function gotPoses(results){
+
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
+    
 }
